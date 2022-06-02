567458c371dff244828a2e1fbd623cd39e48a346
Andre <arc.xoc@gmail.com>
2022-06-01 20:02:04 -0700

    Way More Consise input processor

--- src/helpers/input_processor.py
+++ src/helpers/input_processor.py
@@ -4,111 +4,50 @@ import polars as pl
 import numpy as np
 import helpers.audio_tools as adt
 from lut import clinical_iterables, clinical_data
+import copy 
 
 
-
-def ingestDataV2(data_dir):
+def ingestData(data_dir):
     print("Ingesting data from ", data_dir  )
-
-    iterables = clinical_iterables.copy()
-    data = clinical_data.copy()
-
+    # we use the deepcopy function to avoid overwriting the original clinical_iterables
+    # otherwise this creates a super hard to find bug where running the code multiple times
+    # will result in the output being different each time
+    data = copy.deepcopy(clinical_data)
     for file in os.listdir(data_dir):
         if file.endswith(".txt"):
             # open text file
             with open(data_dir + "/" + file, "r") as f:
+                # read first line
+                line = f.readline()
+                # split line into list
+                line = line.strip().split(" ")
+                data['patient_id'].append(line[0])
+                data['num_locations'].append(line[1])
+                data['sampling_frequency'].append(line[2])
+                
                 # loop through each line to check if it maches with an iterables
+                audio_files = []
+                recording_locations = []
                 for line in f:
-                    for iterable in iterables:
-                        if line.startswith(iterable):
+                    # check if line contains .wav
+                    if ".wav" in line:
+                        # split the line 
+                        line_split = line.split(" ")
+                        audio_files.append(line_split[2]) 
+                        recording_locations.append(line_split[0]) 
+
+
+                    for iterable in clinical_iterables:
+                        if line.startswith(clinical_iterables[iterable] + ":"):
                             # get the value of the iterable
                             value = line.split(': ', 1)[1].strip()
                             # add the value to the data
                             data[iterable].append(value)
-
-#############################################################
-
-def ingestData(data_dir, nan, encode=False): 
-
-    
-    #features listed in feature_names are all listed in the patient txt file using the form "#" + name + ": " + value. Not all features obey this form, so this object does not store all features
-    #stores features and their identifying information as key-value pairs: key = (feature, str), value = (expected text representation of feature in .txt file, str)
-
-
-    #load cipher for encoding features as numbers
-    encode, decode = load_cipher(nan)
-
-    print("Ingesting data from ", data_dir  )
-    
-    # Loop through all text files in the data directory
-    for file in os.listdir(data_dir):
-        if file.endswith(".txt"):
-            # Open text file
-            with open(data_dir + "/" + file, "r") as f:
-
-                #create temporary containers to store features with multiple values
-                patient_audio_files = []
-                patient_recording_locations = []
-
-                #iterate through each line in file
-                for line_number, line in enumerate(f):
-
-                    #get info from first line: first number is patient_id, second number is num_locations, third number is sampling_frequency
-                    if line_number==0:
-                        first_line = line.split(" ")
-                        patient_id, num_locations, sampling_frequency = int(first_line[0]), int(first_line[1]), int(first_line[2])
-                        feature_list['patient_id'].append(patient_id)
-                        feature_list['num_locations'].append(num_locations)
-                        feature_list['sampling_frequency'].append(sampling_frequency)
-
-                    #get audio file names
-                    elif line_number in range(1, num_locations+1):
-                        moving_line = line.strip().split(" ")
-                        current_recording_location, current_audio_file = moving_line[0], moving_line[2]
-
-                        #encode features
-                        if encode:
-                            current_recording_location = encode['recording_locations'][current_recording_location]
-
-                        patient_audio_files.append(current_audio_file)
-                        patient_recording_locations.append(current_recording_location)
-
-                    #get named features
-                    elif line_number>num_locations:
-                        #determine which feature is defined in this line and read data accordingly
-                        for current_named_feature in feature_names.keys():
-                            if line.startswith(feature_names[current_named_feature] + ":"):
-                                val = line.split(': ', 1)[1].strip()
-                                if current_named_feature=='murmur_locations':
-                                    val = val.split('+')
-
-                                #encode features
-                                if encode:
-                                    if current_named_feature in ['height', 'weight']: #convert to int
-                                        if val=='nan':
-                                            val = nan
-                                        else:
-                                            val = int(float(val))
-                                    elif current_named_feature=='murmur_locations': #encode strings in list as numbers
-                                        for i, entry in enumerate(val):
-                                            val[i] = encode[current_named_feature][entry]
-                                    elif not current_named_feature in ['campaign', 'additional_id']: #encode string as number
-                                        val = encode[current_named_feature][val]
-
-                                feature_list[current_named_feature].append(val)
-
-
-                #push to feature_list all features that have not yet been stored there
-                feature_list['audio_files'].append(patient_audio_files)
-                feature_list['recording_locations'].append(patient_recording_locations)
-
-    print('finished reading from files')
-
-    #Create a dataframe to store the data
-    df = pl.DataFrame(feature_list)
-    
-    
+                data['audio_files'].append(audio_files)
+                data['recording_locations'].append(recording_locations)
+    df = pl.DataFrame(data)
     return df
+
 
 #TODO: when exploding audio,files, only some locations have murmurs, address this
 
